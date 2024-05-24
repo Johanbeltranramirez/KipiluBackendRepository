3d0d4e7e20681a5c2934f6715fe5a386b6ff1be9
nicolsita12 <114593458+nicolsita12@users.noreply.github.com>
2024-05-24 12:41:16 -0500

    Update formularioget.js

--- kipilu-api-ver02/models/formularioModel/formularioget.js
+++ kipilu-api-ver02/models/formularioModel/formularioget.js
@@ -1,4 +1,6 @@
-const knex = require('../../config/db/db');
+
+
+   const knex = require('../../config/db/db');
 
 const formularioget = {};
 
@@ -14,5 +16,21 @@ formularioget.getAll = (result) => {
         });
 };
 
-module.exports = formularioget;
+formularioget.getById = (IdForm, result) => {
+    knex.select('*').from('formularios').where('ID_Formulario', IdForm)
+        .then((formularios) => {
+            if (formularios.length) {
+                console.log('Formulario obtenido correctamente: ', formularios[0]);
+                result(null, formularios[0]);
+            } else {
+                console.log('Formulario no encontrado');
+                result({ message: 'Formulario no encontrado' }, null);
+            }
+        })
+        .catch((err) => {
+            console.log('Error al obtener el formulario por ID: ', err);
+            result(err, null);
+        });
+};
 
+module.exports = formularioget;
