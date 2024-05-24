037c59db9ed5b9291b85bcbb7f8153eb14e36630
Johanbeltranramirez <114593412+Johanbeltranramirez@users.noreply.github.com>
2024-05-24 12:35:18 -0500

    Update 20240524011416_sexos_migration.js

--- kipilu-api-ver02/config/db/migrations/20240524011416_sexos_migration.js
+++ kipilu-api-ver02/config/db/migrations/20240524011416_sexos_migration.js
@@ -2,6 +2,13 @@ exports.up = function(knex) {
     return knex.schema.createTable('Sexos', function(table) {
       table.integer('ID_Sexo').unsigned().notNullable().primary();
       table.string('Nombre_Sexo', 10).notNullable();
+    })
+    .then(function() {
+      // Insertar datos
+      return knex('Sexos').insert([
+        { ID_Sexo: 1, Nombre_Sexo: 'Hembra' },
+        { ID_Sexo: 2, Nombre_Sexo: 'Macho' }
+      ]);
     });
   };
   
