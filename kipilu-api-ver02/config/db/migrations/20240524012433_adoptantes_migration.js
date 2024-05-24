5862bde62c81feb3ff330ee0e3b282ec83d9ab6c
Johanbeltranramirez <114593412+Johanbeltranramirez@users.noreply.github.com>
2024-05-24 12:39:30 -0500

    Update 20240524012433_adoptantes_migration.js

--- kipilu-api-ver02/config/db/migrations/20240524012433_adoptantes_migration.js
+++ kipilu-api-ver02/config/db/migrations/20240524012433_adoptantes_migration.js
@@ -8,10 +8,31 @@ exports.up = function(knex) {
       table.string('Correo', 40).notNullable();
       table.string('Direccion', 40).notNullable();
       table.string('Telefono', 20).notNullable();
+    })
+    .then(function() {
+      // Insertar datos
+      return knex('Adoptantes').insert([
+        { 
+          ID_Adoptante: 119020991,
+          P_Nombre: 'Juan',
+          P_Apellido: 'González',
+          Correo: 'juanajjks@gmail.com',
+          Direccion: 'Calle 12 D sur #12-42',
+          Telefono: '3208892991'
+        },
+        { 
+          ID_Adoptante: 119330923,
+          P_Nombre: 'María',
+          P_Apellido: 'López',
+          Correo: 'maria277@hotmail.es',
+          Direccion: 'Avenida Principal 2778 Este',
+          Telefono: '3876543213'
+        }
+      ]);
     });
-  };
+  };  
   
   exports.down = function(knex) {
     return knex.schema.dropTable('Adoptantes');
   };
-    
+      
