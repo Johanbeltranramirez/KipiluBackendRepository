369d92186429cc00f03c9bca787234485a5a7000
Johanbeltranramirez <114593412+Johanbeltranramirez@users.noreply.github.com>
2024-05-24 12:37:51 -0500

    Update 20240524012219_estados_solicitudes_migration.js

--- kipilu-api-ver02/config/db/migrations/20240524012219_estados_solicitudes_migration.js
+++ kipilu-api-ver02/config/db/migrations/20240524012219_estados_solicitudes_migration.js
@@ -2,6 +2,14 @@ exports.up = function(knex) {
     return knex.schema.createTable('Estados_Solicitudes', function(table) {
       table.integer('ID_Solicitud').unsigned().notNullable().primary();
       table.string('Estado_Solicitud', 15).notNullable();
+    })
+    .then(function() {
+      // Insertar datos
+      return knex('Estados_Solicitudes').insert([
+        { ID_Solicitud: 1, Estado_Solicitud: 'Aprobado' },
+        { ID_Solicitud: 2, Estado_Solicitud: 'No Aprobado' },
+        { ID_Solicitud: 3, Estado_Solicitud: 'Pendiente' }
+      ]);
     });
   };
   
