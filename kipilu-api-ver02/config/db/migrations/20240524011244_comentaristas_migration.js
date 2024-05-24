080e4dd5953bcc2bf2dfaf9c86187459fb21be41
Johanbeltranramirez <114593412+Johanbeltranramirez@users.noreply.github.com>
2024-05-24 12:34:33 -0500

    Update 20240524011244_comentaristas_migration.js

--- kipilu-api-ver02/config/db/migrations/20240524011244_comentaristas_migration.js
+++ kipilu-api-ver02/config/db/migrations/20240524011244_comentaristas_migration.js
@@ -4,6 +4,13 @@ exports.up = function(knex) {
       table.string('Nombre', 25).notNullable();
       table.string('Apellido', 25).notNullable();
       table.string('Comentario', 150).notNullable();
+    })
+    .then(function() {
+      // Insertar datos
+      return knex('Comentaristas').insert([
+        { Nombre: 'Juan', Apellido: 'Perez', Comentario: 'QUÉ BEIOSSSSSS AHHHHHHH <3333' },
+        { Nombre: 'María', Apellido: 'García Lorca', Comentario: 'Interesante, gracias por compartir tan nindos perritos' }
+      ]);
     });
   };
   
