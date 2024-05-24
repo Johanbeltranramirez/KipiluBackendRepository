d64822eb238be9d0fbe25b99fe8f2457e4c57c20
Johanbeltranramirez <114593412+Johanbeltranramirez@users.noreply.github.com>
2024-05-24 12:32:18 -0500

    Update 20240524010634_administradores_migration.js

--- kipilu-api-ver02/config/db/migrations/20240524010634_administradores_migration.js
+++ kipilu-api-ver02/config/db/migrations/20240524010634_administradores_migration.js
@@ -5,11 +5,27 @@ exports.up = function(knex) {
       table.string('S_Nombre', 20).nullable();
       table.string('P_Apellido', 20).notNullable();
       table.string('S_Apellido', 20).nullable();
-      table.string('Contrasena', 256).notNullable();
+      table.string('Contrasena', 30).notNullable();
+    })
+    .then(function() {
+      // Insertar datos
+      return knex('Administradores').insert([
+        { 
+          ID_Administrador: 'CC19029900',
+          P_Nombre: 'Paula Andrea',
+          P_Apellido: 'Cruz Zapata',
+          Contrasena: 'ANSDI1998@'
+        },
+        { 
+          ID_Administrador: 'CC1027524883',
+          P_Nombre: 'Johan',
+          P_Apellido: 'Ramírez Beltrán',
+          Contrasena: 'jfrb1234'
+        }
+      ]);
     });
   };
   
   exports.down = function(knex) {
     return knex.schema.dropTable('Administradores');
   };
-  
