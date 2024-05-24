aab227c472338c8aa01c72231b54f2768640d01a
Johanbeltranramirez <114593412+Johanbeltranramirez@users.noreply.github.com>
2024-05-24 12:37:11 -0500

    Update 20240524012113_razas_migration.js

--- kipilu-api-ver02/config/db/migrations/20240524012113_razas_migration.js
+++ kipilu-api-ver02/config/db/migrations/20240524012113_razas_migration.js
@@ -2,6 +2,15 @@ exports.up = function(knex) {
     return knex.schema.createTable('Razas', function(table) {
       table.increments('ID_Raza').primary();
       table.string('Nombre_Raza', 20).notNullable();
+    })
+    .then(function() {
+      // Insertar datos
+      return knex('Razas').insert([
+        { Nombre_Raza: 'Pitbull' },
+        { Nombre_Raza: 'Bulldog' },
+        { Nombre_Raza: 'Siamés' },
+        { Nombre_Raza: 'Carey' }
+      ]);
     });
   };
   
