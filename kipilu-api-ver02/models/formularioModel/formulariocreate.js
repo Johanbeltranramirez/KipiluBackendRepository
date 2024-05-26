e8bd4e33c885b4b289823d769fdc40913c53b669
Johanbeltranramirez <114593412+Johanbeltranramirez@users.noreply.github.com>
2024-05-26 16:08:51 -0500

    Update formulariocreate.js

--- kipilu-api-ver02/models/formularioModel/formulariocreate.js
+++ kipilu-api-ver02/models/formularioModel/formulariocreate.js
@@ -3,24 +3,29 @@ const knex = require('../../config/db/db');
 const formularios = {};
 
 formularios.create = (formulariosData, result) => {
+    console.log(JSON.stringify(formulariosData, null, 2));
     const { Adoptante, Animal } = formulariosData; // Extraemos Adoptante y Animal de los datos del formulario
+
+    // Verifica que los valores de Adoptante y Animal no sean nulos o indefinidos
+    if (!Adoptante || !Animal) {
+        const error = new Error('El campo Adoptante y Animal son obligatorios');
+        console.error('Error al crear el formulario: ', error);
+        result(error, null);
+        return;
+    }
 
     // Definimos los valores predeterminados para Validacion_donativo, Estado_solicitud y Administrador
     const defaultValues = {
         Validacion_donativo: 'NA',
         Estado_solicitud: 3,
-        Administrador: 'CC19029900'
+        Administrador: 'Default'
     };
 
     // Combinamos los valores predeterminados con los datos del formulario
-    const formData = { ...formulariosData, ...defaultValues };
+    const formData = { ...defaultValues, Adoptante, Animal };
 
     knex('formularios')
-        .insert({
-            Adoptante: Adoptante,
-            Animal: Animal,
-            ...formData  // Insertamos todos los datos del formulario
-        })
+        .insert(formData)
         .then((res) => {
             console.log('Id del nuevo formulario: ', res[0]);
             result(null, res[0]);
