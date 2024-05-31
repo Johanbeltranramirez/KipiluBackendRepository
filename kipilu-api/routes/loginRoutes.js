5d3b1a70f725cafc62ee378fcdd92bd8a5978dcc
Johanbeltranramirez <johanframirez13@gmail.com>
2024-05-31 10:49:29 -0500

    cambios

--- kipilu-api/routes/loginRoutes.js
+++
@@ -1,6 +0,0 @@
-const loginController = require('../controllers/loginController');
-
-module.exports = (app) => {
-    // Ruta para el login del administrador
-    app.post('/api/admin/login', loginController.login);
-};
--- kipilu-api/routes/razaRoutes.js
+++
@@ -1,9 +0,0 @@
-const razaController = require('../controllers/razaController');
-
-module.exports = (app) => {
-  // Ruta para crear una raza
-  app.post('/api/razas/create', razaController.createRaza);
-
-  // Ruta para obtener todas las razas
-  app.get('/api/razas', razaController.getAllRazas);
-};
