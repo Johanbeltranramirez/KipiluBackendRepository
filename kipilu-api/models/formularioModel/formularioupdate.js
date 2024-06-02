4474650b4e5e8995eee32859a43f156b8989f036
judynatalialopez <nl311274@gmail.com>
2024-06-02 18:33:46 -0500

    funcional formulario

--- kipilu-api/models/formularioModel/formularioupdate.js
+++ kipilu-api/models/formularioModel/formularioupdate.js
@@ -4,13 +4,13 @@ const formularioupdate = {};
 
 // Método para actualizar un formulario por su ID
 formularioupdate.update = (IdForm, FormData, result) => {
-    const { Adoptante, Animal, Validacion_donativo, Estado_solicitud, Administrador } = FormData;
+    const { ID_Adoptante, ID_Animal, Validacion_donativo, Estado_solicitud, Administrador } = FormData;
 
     knex('Formularios') // Asegúrate de que el nombre de la tabla coincida exactamente con el de la base de datos
         .where('ID_Formulario', IdForm)
         .update({
-            Adoptante: Adoptante,
-            Animal: Animal,
+            ID_Adoptante: ID_Adoptante,
+            ID_Animal: ID_Animal,
             Validacion_donativo: Validacion_donativo,
             Estado_solicitud: Estado_solicitud,
             Administrador: Administrador
