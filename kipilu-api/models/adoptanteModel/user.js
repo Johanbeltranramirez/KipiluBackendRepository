8f910f3c5a62df54498d788213ca92f8c769f56b
judynatalialopez <nl311274@gmail.com>
2024-06-02 17:51:10 -0500

    funcional formulario

--- kipilu-api/models/adoptanteModel/user.js
+++ kipilu-api/models/adoptanteModel/user.js
@@ -1,4 +1,5 @@
 const knex = require('../../config/db/db');
+const AnimalAlter = require('../animalModel/AnimalAlter');
 
 const Adoptante = {};
 
@@ -13,13 +14,45 @@ Adoptante.create = (adoptante, result) => {
             Correo: adoptante.Correo,
             Direccion: adoptante.Direccion,
             Telefono: adoptante.Telefono,
+            ID_Animal: adoptante.ID_Animal
         })
-        .then((res) => {
-            console.log('Id del nuevo adoptante: ', res[0]);
-            result(null, res[0]);
+        .then(() => {
+            console.log('Id del nuevo adoptante: ', adoptante.ID_Adoptante);
+            // Llamar a la función para crear el formulario con el ID del adoptante
+            createFormularioWithAdoptanteID(
+                adoptante.ID_Adoptante,
+                adoptante.ID_Animal,
+                'NA', // Valor predeterminado para Validacion_donativo
+                3,    // Valor predeterminado para Estado_solicitud
+                'Form', // Valor predeterminado para Administrador
+                result
+            );
         })
         .catch((err) => {
             console.log('Error al crear el adoptante: ', err);
+            result(err, null);
+        });
+};
+
+// Función para crear el formulario con el ID del adoptante
+const createFormularioWithAdoptanteID = (ID_Adoptante, ID_Animal, Validacion_donativo, Estado_solicitud, Administrador, result) => {
+    const formulariosData = {
+        ID_Adoptante: ID_Adoptante,
+        ID_Animal: ID_Animal,
+        Validacion_donativo: Validacion_donativo,
+        Estado_solicitud: Estado_solicitud,
+        Administrador: Administrador
+    };
+
+    knex('Formularios')
+        .insert(formulariosData)
+        .then((res) => {
+            console.log('Id del nuevo formulario: ', res[0]);
+            // Actualizar el estado del animal después de crear el formulario
+            AnimalAlter.updateStateById(ID_Animal, result);
+        })
+        .catch((err) => {
+            console.error('Error al crear el formulario: ', err);
             result(err, null);
         });
 };
--- kipilu-api/server.js
+++ kipilu-api/server.js
@@ -37,7 +37,7 @@ loginsuperadminRoutes(app);
 // Dirección IP V4 de la máquina, consultar con ipconfig
 
 
-server.listen(3000,  '192.168.1.7' || 'localhost', function() {
+server.listen(3000,  '192.168.10.16' || 'localhost', function() {
 
 
     console.log('Aplicación de NodeJS ' + process.pid + ' inicio en el puerto ' + port);
