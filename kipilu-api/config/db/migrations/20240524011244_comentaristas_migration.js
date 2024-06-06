e963a1caf6d51aaf52c55e6ed694027109882410
Vanessa Soto <114593363+Vanpan58@users.noreply.github.com>
2024-06-06 13:17:36 -0500

    Update 20240524011244_comentaristas_migration.js

--- kipilu-api/config/db/migrations/20240524011244_comentaristas_migration.js
+++ kipilu-api/config/db/migrations/20240524011244_comentaristas_migration.js
@@ -3,12 +3,12 @@ exports.up = function(knex) {
       table.increments('ID_Comentario').primary();
       table.string('Nombre', 25).notNullable();
       table.string('Apellido', 25).notNullable();
-      table.string('Comentario', 150).notNullable();
+      table.string('Comentario', 70).notNullable();
     })
     .then(function() {
       // Insertar datos
       return knex('Comentaristas').insert([
-        { Nombre: 'Juan', Apellido: 'Perez', Comentario: 'QUÉ BEIOSSSSSS AHHHHHHH <3333' },
+        { Nombre: 'Juan', Apellido: 'Perez', Comentario: 'QUÉ BEIOSSSS AHHHHH <33' },
         { Nombre: 'María', Apellido: 'García Lorca', Comentario: 'Interesante, gracias por compartir tan nindos perritos' }
       ]);
     });
