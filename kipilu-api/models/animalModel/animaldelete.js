fd83f9c3f3752e7a9a9626ca744df038b67d1e15
nicolsita12 <114593458+nicolsita12@users.noreply.github.com>
2024-06-22 15:15:49 -0500

    Update animaldelete.js

--- kipilu-api/models/animalModel/animaldelete.js
+++ kipilu-api/models/animalModel/animaldelete.js
@@ -2,11 +2,25 @@ const knex = require('../../config/db/db');
 
 const AnimalDel = {};
 
-// Método para eliminar un animal por su ID
+// Método para eliminar un animal por su ID solo si está en estado "Adoptado"
 AnimalDel.delete = (animalId, result) => {
-    knex('animales')
+    knex('Animales')
+        .select('Estado_Animal')
         .where('ID_Animal', animalId)
-        .del()
+        .first()
+        .then((animal) => {
+            if (!animal) {
+                return Promise.reject({ message: 'Animal no encontrado' });
+            }
+
+            if (animal.Estado_Animal !== 1) { // Estado_Animal 1 es 'Adoptado'
+                return Promise.reject({ message: 'No se puede eliminar un animal que no esté en estado Adoptado' });
+            }
+
+            return knex('Animales')
+                .where('ID_Animal', animalId)
+                .del();
+        })
         .then(() => {
             console.log('Animal eliminado correctamente');
             result(null, { message: 'Animal eliminado correctamente' });
